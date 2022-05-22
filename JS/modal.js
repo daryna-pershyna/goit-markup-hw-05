4a9b1481ef6c227a2a4e8d3c501f726c32e1d837
Daryna Pershyna <d.pershyna75@gmail.com>
2022-05-22 14:25:51 +0300

    Update modal.js

--- JS/modal.js
+++ JS/modal.js
@@ -6,9 +6,8 @@
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
 })();
